3c6957f9e304333135850aa438cefad99f6323bd
[usherissleepy] <[ashar.zarrar218@gmail.com]>
2025-12-10 13:45:42 -0500

    finished 1 and 2

--- src/1-object-operations.js
+++ src/1-object-operations.js
@@ -1,1 +1,13 @@
-// Create your object here
+let cart = {
+    customerName : "Alex",
+    totalPrice : 0,
+    itemCount : 0
+}
+
+cart.items = ['apple', 'bananas', 'cherries']
+cart.itemCount = 3
+cart.totalPrice = 99.99
+
+delete cart.customerName
+
+console.log(cart)
