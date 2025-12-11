040d593bc818de2fe91fb8c39c1e94fc9f41f6b3
[usherissleepy] <[ashar.zarrar218@gmail.com]>
2025-12-10 21:10:02 -0500

    number 3 done

--- src/3-reference-types.js
+++ src/3-reference-types.js
@@ -4,15 +4,18 @@ const team1 = {
   losses: 20
 };
 
-const team2 = team1;
+const team2 = {...team1}
 team2.wins = 50;
 team2.losses = 15;
 
 /* 
 Part A: What will team1.wins and team1.losses be after this code runs? Write your prediction as a comment below:
 
-- Prediction:
+- Prediction: The wins will be 50 and the losses will be 15
 
 Part B: Fix the code so that team2 is a TRUE COPY of team1. After making your fix, changing team2 should NOT affect team1.
 Part C: Verify your fix by logging both team1.wins (should still be 45) and team2.wins (should be 50)
 */
+
+console.log(`Team 1 wins: ${team1.wins}`)
+console.log(`Team 2 wins: ${team2.wins}`)
